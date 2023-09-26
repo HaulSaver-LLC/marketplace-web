Pita82/web-template

Logo: take logo types into use and add styleguide examples to test it

src/components/Logo/Logo.js
@@ -1,48 +1,123 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { oneOf, string } from 'prop-types';
+import classNames from 'classnames';
 
 import { useConfiguration } from '../../context/configurationContext';
 import { ResponsiveImage } from '../../components/';
 
-const Logo = props => {
-  const config = useConfiguration();
-  const { className, format, ...rest } = props;
-  // NOTE: logo images are set in src/config/brandingConfig.js
-  const { logoImageDesktop, logoImageMobile } = config.branding;
-  const isImageAsset = logo => logo?.type === 'imageAsset';
+import css from './Logo.module.css';
 
-  if (isImageAsset(logoImageDesktop) && format === 'desktop') {
-    const { width, height } = logoImageDesktop.attributes.variants.scaled || {};
+const TYPE24 = 'scaled24h';
+const TYPE36 = 'scaled36h';
+const TYPE48 = 'scaled48h';
+const TYPES = [TYPE24, TYPE36, TYPE48];
+
+// logoType property supports only 3 types at this point
+const isValidLogoType = mode => TYPES.includes(mode);
+const isImageAsset = logo => logo?.type === 'imageAsset';
+
+// Each type can have multiple image variants
+const getVariantNames = variantsObj => {
+  return Object.keys(variantsObj) || [];
+};
+
+// Variant data contains width and height among other things
+// The width is needed for sizes attribute of responsive logo imgs
+const getVariantData = variants => {
+  // This assume that "scaled" variant exists
+  // If other variants are introduced, this setup might need some tuning.
+  return variants['scaled'];
+};
+
+// We have maximum heights for each logo type. It's enforced through classes
+const getHeightClassName = logoType => {
+  return logoType === TYPE48 ? css.logo48 : logoType === TYPE36 ? css.logo36 : css.logo24;
+};
+
+export const LogoComponent = props => {
+  const {
+    className,
+    logoImageClassName,
+    format,
+    marketplaceName,
+    logoImageDesktop,
+    logoImageMobile,
+    logoType,
+    ...rest
+  } = props;
+
+  const hasValidLogoType = isValidLogoType(logoType);
+  const logoClasses = className || css.root;
+  const logoImageClasses = classNames(logoImageClassName || css.logo, getHeightClassName(logoType));
+
+  // Logo from hosted asset
+  if (isImageAsset(logoImageDesktop) && hasValidLogoType && format === 'desktop') {
+    const variants = logoImageDesktop.attributes.variants;
+    const variantNames = getVariantNames(variants);
+    const { width } = getVariantData(variants);
     return (
-      <ResponsiveImage
-        className={className}
-        alt={config.marketplaceName}
-        image={logoImageDesktop}
-        variants={['scaled', 'scaled2x']}
-        sizes={`${width}px`}
-      />
+      <div className={logoClasses}>
+        <ResponsiveImage
+          rootClassName={logoImageClasses}
+          alt={marketplaceName}
+          image={logoImageDesktop}
+          variants={variantNames}
+          sizes={`${width}px`}
+        />
+      </div>
     );
-  } else if (isImageAsset(logoImageMobile) && format === 'mobile') {
-    const { width, height } = logoImageMobile.attributes.variants.scaled || {};
+  } else if (isImageAsset(logoImageMobile) && hasValidLogoType && format === 'mobile') {
+    const variants = logoImageMobile.attributes.variants;
+    const variantNames = getVariantNames(variants);
+    const { width } = getVariantData(variants);
+
+    // Sizes on small screens are mainly limited by space: side buttons take 2x66 px, the rest is for logo.
+    // If logo's (1x) width is less than 188, we can use logo's width as limit for sizes attribute
+    // On general case, up to the screen size of 500px, we could say that the logo _might_ take all the available space
+    // However, after 500px, the max aspect ratio for the logo should start limiting the logo's width.
+    const sizes =
+      width <= 188 ? `${width}px` : `(max-width: 500px) calc(100vw - 132px), ${width}px`;
     return (
-      <ResponsiveImage
-        className={className}
-        alt={config.marketplaceName}
-        image={logoImageMobile}
-        variants={['scaled', 'scaled2x']}
-        sizes={`${width}px`}
-      />
+      <div className={logoClasses}>
+        <ResponsiveImage
+          rootClassName={logoImageClasses}
+          alt={marketplaceName}
+          image={logoImageMobile}
+          variants={variantNames}
+          sizes={sizes}
+        />
+      </div>
     );
   } else if (format === 'desktop') {
     return (
-      <img className={className} src={logoImageDesktop} alt={config.marketplaceName} {...rest} />
+      <div className={logoClasses}>
+        <img className={logoImageClasses} src={logoImageDesktop} alt={marketplaceName} {...rest} />
+      </div>
     );
   }
 
-  return <img className={className} src={logoImageMobile} alt={config.marketplaceName} {...rest} />;
+  return (
+    <div className={logoClasses}>
+      <img className={logoImageClasses} src={logoImageMobile} alt={marketplaceName} {...rest} />
+    </div>
+  );
 };
 
-const { oneOf, string } = PropTypes;
+const Logo = props => {
+  const config = useConfiguration();
+  // NOTE: logo images are set in hosted branding.json asset or src/config/brandingConfig.js
+  const { logoImageDesktop, logoImageMobile, logoType } = config.branding;
+
+  return (
+    <LogoComponent
+      {...props}
+      logoImageDesktop={logoImageDesktop}
+      logoImageMobile={logoImageMobile}
+      logoType={logoType}
+      marketplaceName={config.marketplaceName}
+    />
+  );
+};
 
 Logo.defaultProps = {
   className: null,

HaulSaver-LLC/marketplace-web

src/components/Logo/Logo.js
@@ -1,48 +1,127 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { oneOf, string } from 'prop-types';
+import classNames from 'classnames';
 
 import { useConfiguration } from '../../context/configurationContext';
 import { ResponsiveImage } from '../../components/';
 
-const Logo = props => {
-  const config = useConfiguration();
-  const { className, format, ...rest } = props;
-  // NOTE: logo images are set in src/config/brandingConfig.js
-  const { logoImageDesktop, logoImageMobile } = config.branding;
-  const isImageAsset = logo => logo?.type === 'imageAsset';
+import css from './Logo.module.css';
 
-  if (isImageAsset(logoImageDesktop) && format === 'desktop') {
-    const { width, height } = logoImageDesktop.attributes.variants.scaled || {};
+const HEIGHT_24 = 24;
+const HEIGHT_36 = 36;
+const HEIGHT_48 = 48;
+const HEIGHT_OPTIONS = [HEIGHT_24, HEIGHT_36, HEIGHT_48];
+
+// logoSettings property supports only 3 types at this point
+const isValidLogoSettings = settings =>
+  settings?.format === 'image' && HEIGHT_OPTIONS.includes(settings?.height);
+const isImageAsset = logo => logo?.type === 'imageAsset';
+
+// Each type can have multiple image variants
+const getVariantNames = variantsObj => {
+  return Object.keys(variantsObj) || [];
+};
+
+// Variant data contains width and height among other things
+// The width is needed for sizes attribute of responsive logo imgs
+const getVariantData = variants => {
+  // This assume that "scaled" variant exists
+  // If other variants are introduced, this setup might need some tuning.
+  return variants['scaled'];
+};
+
+// We have maximum heights for each logo type. It's enforced through classes
+const getHeightClassName = height => {
+  return height === HEIGHT_48 ? css.logo48 : height === HEIGHT_36 ? css.logo36 : css.logo24;
+};
+
+export const LogoComponent = props => {
+  const {
+    className,
+    logoImageClassName,
+    format,
+    marketplaceName,
+    logoImageDesktop,
+    logoImageMobile,
+    logoSettings,
+    ...rest
+  } = props;
+
+  const hasValidLogoSettings = isValidLogoSettings(logoSettings);
+  const logoClasses = className || css.root;
+  const logoImageClasses = classNames(
+    logoImageClassName || css.logo,
+    getHeightClassName(logoSettings?.height)
+  );
+
+  // Logo from hosted asset
+  if (isImageAsset(logoImageDesktop) && hasValidLogoSettings && format === 'desktop') {
+    const variants = logoImageDesktop.attributes.variants;
+    const variantNames = getVariantNames(variants);
+    const { width } = getVariantData(variants);
     return (
-      <ResponsiveImage
-        className={className}
-        alt={config.marketplaceName}
-        image={logoImageDesktop}
-        variants={['scaled', 'scaled2x']}
-        sizes={`${width}px`}
-      />
+      <div className={logoClasses}>
+        <ResponsiveImage
+          rootClassName={logoImageClasses}
+          alt={marketplaceName}
+          image={logoImageDesktop}
+          variants={variantNames}
+          sizes={`${width}px`}
+        />
+      </div>
     );
-  } else if (isImageAsset(logoImageMobile) && format === 'mobile') {
-    const { width, height } = logoImageMobile.attributes.variants.scaled || {};
+  } else if (isImageAsset(logoImageMobile) && hasValidLogoSettings && format === 'mobile') {
+    const variants = logoImageMobile.attributes.variants;
+    const variantNames = getVariantNames(variants);
+    const { width } = getVariantData(variants);
+
+    // Sizes on small screens are mainly limited by space: side buttons take 2x66 px, the rest is for logo.
+    // If logo's (1x) width is less than 188, we can use logo's width as limit for sizes attribute
+    // On general case, up to the screen size of 500px, we could say that the logo _might_ take all the available space
+    // However, after 500px, the max aspect ratio for the logo should start limiting the logo's width.
+    const sizes =
+      width <= 188 ? `${width}px` : `(max-width: 500px) calc(100vw - 132px), ${width}px`;
     return (
-      <ResponsiveImage
-        className={className}
-        alt={config.marketplaceName}
-        image={logoImageMobile}
-        variants={['scaled', 'scaled2x']}
-        sizes={`${width}px`}
-      />
+      <div className={logoClasses}>
+        <ResponsiveImage
+          rootClassName={logoImageClasses}
+          alt={marketplaceName}
+          image={logoImageMobile}
+          variants={variantNames}
+          sizes={sizes}
+        />
+      </div>
     );
   } else if (format === 'desktop') {
     return (
-      <img className={className} src={logoImageDesktop} alt={config.marketplaceName} {...rest} />
+      <div className={logoClasses}>
+        <img className={logoImageClasses} src={logoImageDesktop} alt={marketplaceName} {...rest} />
+      </div>
     );
   }
 
-  return <img className={className} src={logoImageMobile} alt={config.marketplaceName} {...rest} />;
+  return (
+    <div className={logoClasses}>
+      <img className={logoImageClasses} src={logoImageMobile} alt={marketplaceName} {...rest} />
+    </div>
+  );
 };
 
-const { oneOf, string } = PropTypes;
+const Logo = props => {
+  const config = useConfiguration();
+  // NOTE: logo images are set in hosted branding.json asset or src/config/brandingConfig.js
+  const { logoImageDesktop, logoImageMobile, logoSettings } = config.branding;
+
+  return (
+    <LogoComponent
+      {...props}
+      logoImageDesktop={logoImageDesktop}
+      logoImageMobile={logoImageMobile}
+      logoSettings={logoSettings}
+      marketplaceName={config.marketplaceName}
+    />
+  );
+};
 
 Logo.defaultProps = {
   className: null,