devppmc/web-template

Add viewing rights option to NoAccessPage

src/util/urlHelpers.js
@@ -21,6 +21,8 @@ export const PROFILE_PAGE_PENDING_APPROVAL_VARIANT = 'pending-approval';
 export const NO_ACCESS_PAGE_POST_LISTINGS = 'posting-right';
 // If user account is on pending-approval state, then user can't initiate transactions or create listings
 export const NO_ACCESS_PAGE_USER_PENDING_APPROVAL = 'user-approval';
+// If user account has no viewing rights on a private marketplace, they can only see their own listings
+export const NO_ACCESS_PAGE_VIEW_LISTINGS = 'viewing-right';
 
 // Create slug from random texts
 // From Gist thread: https://gist.github.com/mathewbyrne/1280286

HaulSaver-LLC/marketplace-web

src/util/urlHelpers.js
@@ -22,6 +22,8 @@ export const NO_ACCESS_PAGE_POST_LISTINGS = 'posting-rights';
 export const NO_ACCESS_PAGE_INITIATE_TRANSACTIONS = 'transaction-rights';
 // If user account is on pending-approval state, then user can't initiate transactions or create listings
 export const NO_ACCESS_PAGE_USER_PENDING_APPROVAL = 'user-approval';
+// If user account has no viewing rights on a private marketplace, they can only see their own listings
+export const NO_ACCESS_PAGE_VIEW_LISTINGS = 'viewing-rights';
 
 // Create slug from random texts
 // From Gist thread: https://gist.github.com/mathewbyrne/1280286