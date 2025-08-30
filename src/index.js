db80dc59ef5dbee8eda169754991d08f3d02dc8b
EdwardSomeraWebDev <edsomera14@gmail.com>
2025-08-30 08:11:42 +0800

    Fix build error for mapbox

--- src/index.js
+++ src/index.js
@@ -15,7 +15,9 @@
 import React from 'react';
 import ReactDOMClient from 'react-dom/client';
 import { loadableReady } from '@loadable/component';
-import 'mapbox-gl/dist/mapbox-gl.css';
+if (typeof window !== 'undefined') {
+  import('mapbox-gl/dist/mapbox-gl.css');
+}
 
 // Import default styles before other CSS-related modules are imported
 // This ensures that the styles in marketplaceDefaults.css are included
